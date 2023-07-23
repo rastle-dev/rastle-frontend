fbbb729cc451424725495e8439f9e1b11e0a2b40
miin-hyukkk <ham9893@naver.com>
2023-07-23 19:05:53 +0900

    feat: 색깔 관련 상수 구현

---
+++ src/constants/color.ts
@@ -0,0 +1,16 @@
+const COLORS = {
+  WHITE: "#FFF",
+  BLACK: "#000",
+  GREY: {
+    50: "#F9F9F9",
+    100: "#F5F5F9",
+    200: "#E9E9EE",
+    300: "#C6C6D0",
+    400: "#9090A0",
+    500: "#626273",
+    600: "#464656",
+  },
+  BLUE: "#7798FC",
+};
+
+export default COLORS;
