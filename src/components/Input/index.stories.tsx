bae047b9ac327bfff16f677cca3b6ae02d710149
miin-hyukkk <ham9893@naver.com>
2023-07-27 19:51:23 +0900

    feat: storybook 인풋 컴포넌트 구현

---
+++ src/components/Input/index.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { withKnobs } from "@storybook/addon-knobs";
+import Input from "./index";
+
+export default {
+  title: "components/Input", // 스토리북에서 보여질 그룹과 경로를 명시
+  component: Input, // 어떤 컴포넌트를 문서화 할지 명시
+  decorators: [withKnobs], // 애드온 적용
+};
+
+export function DefaultInput() {
+  return <Input value="TEST" labelHidden />;
+}
+export function CheckBox() {
+  return <Input type="checkbox" />;
+}
+export function LabelInput() {
+  return <Input size={30} value="label" label="label" />;
+}
+export function ControlTest(args: any) {
+  return <Input {...args} />;
+}
+ControlTest.args = {
+  size: "30rem",
+};
