adf03ba4cdb337ef0049f5450063e45e351e621b
miin-hyukkk <ham9893@naver.com>
2023-07-30 19:19:37 +0900

    feat: 글로벌폰트, 미디어쿼리 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,105 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  font-family: "EscoreDream", sans-serif ;
+  font-size: 100%;
+  padding: 0;
+  margin: 0;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+* {
+font-family: "EscoreDream", sans-serif ;
+  font-weight: 500;
+  box-sizing: border-box;
+}
+@media (min-width: 768px) and (max-width: 1024px) {
+  html {
+    font-size: 13px;
+  }
+}
+@media (min-width: 1025px) and (max-width: 1440px) {
+  html {
+    font-size: 14px;
+  }
+}
+@media (min-width: 1441px) and (max-width: 1919px) {
+  html {
+    font-size: 16px;
+  }
+}
+@media (min-width: 1920px) and (max-width: 2559px) {
+  html {
+    font-size: 17px;
+  }
+}
+@media (min-width: 2560px) {
+  html {
+    font-size: 18px;
+  }
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream1.otf") format('opentype');
+  font-weight: 800;
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream2.otf") format('opentype');
+  font-weight: 200;
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream3.otf") format('opentype');
+  font-weight: 300;
+
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream4.otf") format('opentype');
+  font-weight: 400;
+
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream5.otf") format('opentype');
+  font-weight: 500;
+
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream6.otf") format('opentype');
+  font-weight: 600;
+
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream7.otf") format('opentype');
+  font-weight: 700;
+
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream8.otf") format('opentype');
+  font-weight: 800;
+
+}
+@font-face {
+  font-family: 'EscoreDream';
+  src: url("/font/SCDream9.otf") format('opentype');
+  font-weight: 900;
+}
+`;
 
 const queryClient = new QueryClient();
 
@@ -12,6 +107,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </Hydrate>
     </QueryClientProvider>
