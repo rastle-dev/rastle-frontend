979ddadfac1ef7f7d069e459d24410b55e1f8d73
miin-hyukkk <ham9893@naver.com>
2023-08-03 23:00:38 +0900

    feat: 소셜로그인 UI 구현

---
+++ src/components/Login/sns.tsx
@@ -0,0 +1,20 @@
+import * as S from "@/pages/login/index.styles";
+import React from "react";
+
+export default function SNSLogin() {
+  return (
+    <S.SNSLogin>
+      <S.NAVERLogo>
+        <img src="/naver.png" alt="네이버 로고" />
+        <div>네이버로 로그인하기</div>
+      </S.NAVERLogo>
+      <S.KAKAOLogo>
+        <img
+          src="https://cdn.imweb.me/upload/S20210304872ba49a108a8/89a68d1e3674a.png"
+          alt="카카오 로고"
+        />
+        <div>카카오로 로그인하기</div>
+      </S.KAKAOLogo>
+    </S.SNSLogin>
+  );
+}
