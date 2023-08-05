7ec0a0dc3f16ea87302406e0c72bc01e13fca86f
miin-hyukkk <ham9893@naver.com>
2023-08-06 00:42:13 +0900

    feat: 모바일 반응형 구현

--- src/pages/shop/index.styles.tsx
+++ src/pages/shop/index.styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import COLORS from "@/constants/color";
+import media from "@/styles/media";
 
 export const Container = styled.div`
   width: 100%;
@@ -9,10 +10,9 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.div`
-  width: 92%;
-  position: fixed;
+  width: 88%;
+  //position: fixed;
   background-color: ${COLORS.WHITE};
-
   h1 {
     font-size: 2.5rem;
     font-weight: 200;
@@ -20,34 +20,48 @@ export const Header = styled.div`
   }
 `;
 export const Line = styled.div`
-  border: 0.5px solid;
+  border-bottom: 0.5px solid;
   color: ${COLORS.GREY[200]};
-  padding-top: 20.3rem;
+  //padding-top: 20.4rem;
   width: 100%;
 `;
 export const FirstMarketDescription = styled.div`
   padding-top: 3rem;
-  width: 92%;
+  width: 88%;
   font-weight: 200;
+  ${media.xsmall} {
+    width: 95%;
+  }
+  ${media.small} {
+    width: 90%;
+  }
   font-size: 2rem;
 `;
 
 export const ProductList = styled.div`
   display: grid;
-  width: 92%;
-  grid-template-columns: repeat(auto-fill, minmax(23%, auto));
+  width: 88%;
+  grid-template-columns: repeat(4, 1fr);
+  ${media.xsmall} {
+    grid-template-columns: repeat(2, 1fr);
+    row-gap: 2.5rem;
+    width: 95%;
+  }
+  ${media.small} {
+    grid-template-columns: repeat(auto-fill, minmax(30%, auto));
+    row-gap: 2.5rem;
+    width: 90%;
+  }
   padding-top: 4.8rem;
-
   column-gap: 2rem;
   row-gap: 8rem;
 `;
 export const Product = styled.div`
-  height: 43.25rem;
   background-color: transparent;
+  width: 100%;
 `;
 export const Img = styled.img`
-  height: 32.375rem;
-  width: 28.6rem;
+  width: 100%;
 `;
 export const Price = styled.div`
   font-size: 1.5rem;
@@ -58,6 +72,7 @@ export const Name = styled.div`
   font-size: 1.5rem;
   padding-top: 1rem;
   font-weight: 200;
+  width: 100%;
 `;
 export const Event = styled.div`
   font-size: 1.5rem;
