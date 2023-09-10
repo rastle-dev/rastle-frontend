166a6d4153cebb5de922be3992fc3adc00b73393
miin-hyukkk <ham9893@naver.com>
2023-09-10 15:30:25 +0900

    chore: 콘솔 제거

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,6 @@
 import { authorizationClient, unAuthorizationClient } from ".";
 import API from "./config";
 
-// eslint-disable-next-line import/prefer-default-export
 export const authLogin = async (userLoginData: object) => {
   const { headers } = await unAuthorizationClient.post(
     API.LOGIN,
