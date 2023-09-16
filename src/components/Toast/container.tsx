636661c05f1c534b986ad638904422de4537c714
miin-hyukkk <ham9893@naver.com>
2023-09-16 17:35:31 +0900

    feat: 토스트 반응형 구현

--- src/components/Toast/container.tsx
+++ src/components/Toast/container.tsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import media from "@/styles/media";
 
 const StyledContainer = styled(ToastContainer)`
   &&.Toastify__toast-container {
     padding: 0;
     width: auto;
+    ${media.mobile} {
+      position: fixed;
+      top: 3rem;
+      left: 50%;
+      width: 70%;
+      transform: translateX(-50%);
+    }
     max-width: 575px;
     height: 5rem;
     text-align: center;
