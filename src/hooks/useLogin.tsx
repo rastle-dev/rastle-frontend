4645451f6f8ea1a22e1ffc0ed63c4ecb39698067
miin-hyukkk <ham9893@naver.com>
2023-09-24 15:54:37 +0900

    feat: 로그아웃 api 마이페이지 훅으로 분리

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -2,10 +2,9 @@ import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 import useInput from "@/hooks/useInput";
-import { authLogin, authLogout } from "@/api/auth";
+import { authLogin } from "@/api/auth";
 import PATH from "@/constants/path";
 import errorMsg from "@/components/Toast/error";
-import toastMsg from "@/components/Toast";
 
 export default function useLogin() {
   const router = useRouter();
@@ -31,17 +30,6 @@ export default function useLogin() {
       console.log(`${errorCode} / ${message}`);
     },
   });
-  const logout = async () => {
-    try {
-      console.log("local", localStorage);
-      await authLogout();
-      localStorage.clear();
-      toastMsg("로그아웃 되었습니다!");
-      router.push(PATH.HOME);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return {
     mutateLogin,
@@ -49,6 +37,5 @@ export default function useLogin() {
     onChangePassword,
     email,
     onChangeEmail,
-    logout,
   };
 }
