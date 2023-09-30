a30a8b2bdc7cbc0e3f638f8bf1464172adc2e55e
euisung <ghddmltjd11@gmail.com>
2023-09-30 22:36:15 +0900

    refactor: 관리자 페이지 카테고리 생성 리팩토링(조회추가)

--- src/components/Manager/category/CreateCategory.tsx
+++ src/components/Manager/category/CreateCategory.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import useCreateCategory from "@/hooks/manager/category/useCreateCategory";
 import Input from "@/components/common/Input";
+import { useQuery } from "@tanstack/react-query";
+import QUERYKEYS from "@/constants/querykey";
+import { adminGetCategory } from "@/api/admin";
 
 const Title = styled.div`
   margin: 0;
@@ -18,12 +21,31 @@ const CategoryDetail = styled.div`
   }
 `;
 
+interface Category {
+  id: number;
+  name: string;
+}
+
 export default function CreateCategory() {
-  const { createCategory, onChangeName, name } = useCreateCategory();
-  console.log(name);
+  const { createCategory, onChangeName } = useCreateCategory();
+
+  const { data: categoryData } = useQuery(
+    [QUERYKEYS.ADMIN_GET_CATEGORY],
+    adminGetCategory,
+  );
+
+  console.log(categoryData);
+
   return (
     <div>
       <Title>카테고리 추가</Title>
+      <CategoryDetail>
+        <h3>현재 카테고리</h3>
+        {categoryData?.data.map((category: Category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </CategoryDetail>
+      <p>상의, 하의, 악세서리, 아우터 등을 추가하세요</p>
       <CategoryDetail>
         <Input size={30} onChange={onChangeName} />
       </CategoryDetail>
--- src/hooks/manager/category/useCreateCategory.tsx
+++ src/hooks/manager/category/useCreateCategory.tsx
@@ -2,12 +2,21 @@ import { adminCreateCategory } from "@/api/admin";
 import useInput from "@/hooks/useInput";
 
 export default function useCreateCategory() {
-  const [name, onChangeName] = useInput("");
+  const [name, onChangeName, setName] = useInput("");
 
+  console.log(name);
   const createCategory = async () => {
     try {
-      const data = await adminCreateCategory({ name });
-      console.log(data);
+      const shouldCreate = window.confirm(`'${name}'을(를) 추가하시겠습니까?`);
+
+      if (shouldCreate) {
+        const data = await adminCreateCategory({ name });
+        console.log(data);
+        alert("카테고리 생성을 성공했습니다");
+        setName("");
+      } else {
+        alert("카테고리 추가가 취소되었습니다");
+      }
     } catch (error) {
       console.error("카테고리 생성 중 오류 발생:", error);
     }
