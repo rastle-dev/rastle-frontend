5f963ccba83c53528ff97c698cf77c895fd9d5fa
miin-hyukkk <ham9893@naver.com>
2023-10-16 20:45:05 +0900

    chore: Eslint 오류 수정

--- src/pages/mypage/index.styles.tsx
+++ src/pages/mypage/index.styles.tsx
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 import COLORS from "@/constants/color";
-import Input from "@/components/common/Input";
 import Button from "@/components/common/Button";
 
-// eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
   width: 100%;
   margin-top: 5.5rem;
@@ -18,7 +16,6 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   display: flex;
   border-top: 1px solid ${COLORS.GREY[200]};
-  //border-top: 1px solid #e6e6e6;
 `;
 export const Sidebar = styled.div`
   flex: 17;
@@ -26,8 +23,6 @@ export const Sidebar = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 4.1rem;
-  //border: 1px solid red;
-  //border-right: 1px solid ${COLORS.GREY[200]};
   padding-top: 4.1rem;
 `;
 export const Content = styled.div`
@@ -62,7 +57,9 @@ export const Logout = styled(Button)`
     font-weight: 500;
   }
 `;
-export const Box = styled.div``;
+export const Box = styled.div`
+  border: none;
+`;
 export const SubMenu = styled.div<{ active: boolean }>`
   color: ${(props) => (props.active ? COLORS.블랙 : COLORS.GREY[300])};
   font-size: 1.36rem;
@@ -82,79 +79,5 @@ export const TabMenu = styled.div`
     font-weight: 200;
     cursor: pointer;
     margin: 0 0 1.4rem 0;
-  }
-`;
-export const Table = styled.div`
-  border-bottom: 1px solid;
-  width: 90.5rem;
-`;
-export const Select = styled(Input)`
-  width: 2rem;
-`;
-export const TableHeader = styled.div`
-  border-bottom: 1px solid;
-  display: grid;
-  align-items: center;
-  grid-template-columns: 17rem 22rem 12.5rem 11rem 12.5rem 11rem 5rem;
-  p {
-    margin: 1rem 0 1rem 0;
-    font-size: 1.45rem;
-    font-weight: 500;
-  }
-`;
-export const TableContent = styled.div`
-  //display: grid;
-  grid-template-columns: 15rem 19rem 8rem 8rem 8rem 8rem;
-`;
-export const ProductInfo = styled.div`
-  display: grid;
-  grid-template-columns: 3.2rem 10rem 25rem 13.8rem 9.8rem 11.5rem 11rem 6.2rem;
-  align-items: center;
-  margin: 1.9rem 0 1.9rem 0;
-  p {
-    font-size: 1.45rem;
-    font-weight: 200;
-  }
-`;
-export const Img = styled.img`
-  width: 7.5rem;
-  height: 8.2rem;
-`;
-export const TextInfo = styled.div`
-  width: 24rem;
-  padding-right: 1rem;
-  p {
-    font-size: 1.45rem;
-    font-weight: 500;
-    margin: 0;
-  }
-`;
-export const SelectTab = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.36rem;
-`;
-export const SelectButton = styled(Button)`
-  margin: 0;
-  text-align: center;
-  padding: 0.5rem 0 0.5rem 0;
-  font-weight: 200;
-  font-size: 1rem;
-  width: 6.2rem;
-  border-radius: 0;
-  border: 0.5px solid;
-`;
-export const TotalPrice = styled.div`
-  display: flex;
-  justify-content: right;
-  font-size: 1.45rem;
-  padding: 2.2rem 0 2.2rem 0;
-  width: 90.5rem;
-  p {
-    margin: 0;
-    font-weight: 200;
-    padding: 0 0.5rem 0 0.5rem;
-  }
-  div {
   }
 `;
