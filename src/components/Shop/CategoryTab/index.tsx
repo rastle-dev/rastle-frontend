75201be539cde783ddcf382f7dbf85adba2e4171
miin-hyukkk <ham9893@naver.com>
2023-11-11 18:23:43 +0900

    design: 카테고리 탭 반응형 구현

--- src/components/Shop/CategoryTab/index.tsx
+++ src/components/Shop/CategoryTab/index.tsx
@@ -4,6 +4,7 @@ import COLORS from "@/constants/color";
 import { useQuery } from "@tanstack/react-query";
 import QUERYKEYS from "@/constants/querykey";
 import { adminGetCategory } from "@/api/admin";
+import media from "@/styles/media";
 
 type ProductCategory = "전체" | "코디상품" | "상의" | "하의" | "이벤트";
 
@@ -16,6 +17,16 @@ interface ProductCategoryTabsProps {
 const TabContainer = styled.div`
   display: flex;
   gap: 6rem;
+  -webkit-overflow-scrolling: touch;
+
+  @media (max-width: 768px) {
+    overflow-x: auto;
+    -webkit-overflow-scrolling: touch; /* iOS 스크롤 부드럽게 만들기 */
+    flex-wrap: nowrap; /* 가로로 스크롤 가능하도록 설정 */
+    justify-content: flex-start;
+    gap: 3rem;
+  }
+  width: 100%;
 `;
 
 const TabItem = styled.div<{ active: boolean }>`
@@ -27,6 +38,9 @@ const TabItem = styled.div<{ active: boolean }>`
   padding-bottom: 3.375rem;
   font-weight: 200;
   font-size: 1.3rem;
+  @media (max-width: 768px) {
+    flex: 0 0 auto; /* 가로로 스크롤 가능한 상태에서 너비를 자동으로 설정 */
+  }
 `;
 export default function ProductCategoryTabs({
   categories,
