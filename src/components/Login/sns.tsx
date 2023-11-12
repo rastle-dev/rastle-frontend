e105e3aba8c3d62af5913bf6464fa5bf5c426db4
miin-hyukkk <ham9893@naver.com>
2023-11-12 16:17:37 +0900

    feat: 카카오 로그인 기능 구현 테스트

--- src/components/Login/sns.tsx
+++ src/components/Login/sns.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "@/styles/login/index.styles";
+import API from "@/api/config";
 
 export default function SNSLogin() {
+  const [currentURL, setCurrentURL] = useState("");
+
+  useEffect(() => {
+    const url = window.location.href;
+    setCurrentURL(url);
+    // 현재 URL에 대한 작업 수행
+  }, []);
+  console.log("current", currentURL);
+  useEffect(() => {
+    // URL에서 인가 코드 값을 추출.
+    const searchParams = new URLSearchParams(window.location.search);
+    console.log("searchParams", searchParams);
+  }, []);
+
   return (
     <S.SNSLogin>
+      {/* eslint-disable-next-line react/jsx-no-bind */}
       <S.NAVERLogo>
         <img src="/naver.png" alt="네이버 로고" />
         <div>네이버로 로그인하기</div>
       </S.NAVERLogo>
-      <S.KAKAOLogo>
+      <S.KAKAOLogo
+        onClick={() => {
+          console.log("click");
+          window.location.href = currentURL.includes("localhost:3000")
+            ? API.LOCAL_KAKAO_AUTH_URL
+            : API.KAKAO_AUTH_URL;
+        }}
+      >
         <img
           src="https://cdn.imweb.me/upload/S20210304872ba49a108a8/89a68d1e3674a.png"
           alt="카카오 로고"
