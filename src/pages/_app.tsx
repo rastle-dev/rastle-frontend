96d5c26257b06908991e52c2bc91ebdb1659cf6c
miin-hyukkk <ham9893@naver.com>
2023-11-30 15:45:31 +0900

    feat: 전체 데이터, 카테고리 데이터 -> SSG with revalidate으로 변경 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { createGlobalStyle } from "styled-components";
 import DefaultLayout from "@/components/Layout/DefaultLayout";
 import MainLayout from "@/components/Layout/MainLayout";
@@ -97,6 +99,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <GlobalStyle />
         {Layout}
       </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
