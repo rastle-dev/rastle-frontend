c40bdd854e52e0f97bd6cbfc2b4d6d156cdd5c51
miin-hyukkk <ham9893@naver.com>
2023-12-17 19:40:11 +0900

    chore: 페이징 관련 함수 삭제

--- src/hooks/useShop.tsx
+++ src/hooks/useShop.tsx
@@ -1,14 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { loadBundle, loadSelectBundle } from "@/api/shop";
+import { loadSelectBundle } from "@/api/shop";
 import QUERYKEYS from "@/constants/querykey";
 
 export default function useShop() {
-  const useLoadBundle = (BundleData: object) => {
-    const queryFn = () => loadBundle(BundleData);
-    const { data, refetch } = useQuery([QUERYKEYS.LOAD_BUNDLE], queryFn);
-    return { data, refetch };
-  };
   const useLoadSelectBundle = (bundleId: number) => {
     const queryFn = () => loadSelectBundle(bundleId);
     const { data, refetch } = useQuery(
@@ -17,18 +11,8 @@ export default function useShop() {
     );
     return { data, refetch };
   };
-  const ITEM_SIZE = 2;
-  const [curPage, setCurPage] = useState(1);
 
-  const onChangePage = (page: number) => {
-    setCurPage(page);
-    console.log("클릭");
-  };
   return {
-    useLoadBundle,
-    onChangePage,
-    curPage,
-    ITEM_SIZE,
     useLoadSelectBundle,
   };
 }
