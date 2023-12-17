6552dc1bb9315c8a92b46612e8595af1f2aa3fa2
miin-hyukkk <ham9893@naver.com>
2023-12-17 19:40:49 +0900

    feat: 코디상품 무한 스크롤 구현

--- src/components/Shop/CodyProduct.tsx
+++ src/components/Shop/CodyProduct.tsx
@@ -1,44 +1,70 @@
 import React, { useEffect } from "react";
-import Pagination from "react-js-pagination";
 import * as S from "@/styles/shop/index.styles";
 import SwiperComponent from "@/components/Shop/codySwiper";
-import useShop from "@/hooks/useShop";
-import { PageNumberContainer } from "@/components/Manager/UserManagement";
 import DisplaySelectProduct from "@/components/Shop/DisplaySelectProduct";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import QUERYKEYS from "@/constants/querykey";
+import { loadBundle } from "@/api/shop";
+import {
+  LoadingSpinner,
+  LoadingSpinnerWrapper,
+} from "@/components/LoadingSpinner";
 
 export default function CodyProduct() {
-  const { useLoadBundle, ITEM_SIZE, curPage, onChangePage } = useShop();
-  const { data: bundleData, refetch } = useLoadBundle({
-    page: curPage - 1,
-    size: ITEM_SIZE,
-  });
+  const {
+    data: infiniteData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    [QUERYKEYS.LOAD_BUNDLE], // 쿼리 키
+    ({ pageParam = 0 }) => loadBundle({ page: pageParam, size: 1 }),
+    {
+      getNextPageParam: (lastPage) => {
+        const nextPage = lastPage.data.pageable.pageNumber + 1;
+        return nextPage < lastPage.data.totalPages ? nextPage : undefined;
+      },
+    },
+  );
 
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+      hasNextPage &&
+      !isFetchingNextPage
+    ) {
+      fetchNextPage();
+    }
+  };
+
+  // useEffect를 사용하여 스크롤 이벤트 리스너 등록
   useEffect(() => {
-    refetch();
-  }, [curPage, bundleData]);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
   return (
     <>
       <S.SetBox>
-        {bundleData?.data.content.map((item: any, idx: number) => (
-          <S.CurrentMarketWrapper>
-            <S.FirstMarketDescription>
-              RECORDY {idx + 1}: {item.name}
-            </S.FirstMarketDescription>
-            <SwiperComponent imgUrls={item.imageUrls} />
-            <h2>제품 정보</h2>
-            {item.id && <DisplaySelectProduct id={item.id} />}
-          </S.CurrentMarketWrapper>
-        ))}
+        {infiniteData?.pages.flatMap((page: any, idx: number) =>
+          page.data.content.map((item: any) => (
+            <S.CurrentMarketWrapper key={item.id}>
+              <S.FirstMarketDescription>
+                RECORDY {idx + 1}: {item.name}
+              </S.FirstMarketDescription>
+              <SwiperComponent imgUrls={item.imageUrls} />
+              <h2>제품 정보</h2>
+              {item.id && <DisplaySelectProduct id={item.id} />}
+            </S.CurrentMarketWrapper>
+          )),
+        )}
       </S.SetBox>
-      <PageNumberContainer>
-        <Pagination
-          activePage={curPage}
-          itemsCountPerPage={ITEM_SIZE}
-          totalItemsCount={bundleData?.data.totalElements || 1}
-          pageRangeDisplayed={5}
-          onChange={onChangePage}
-        />
-      </PageNumberContainer>
+      {isFetchingNextPage && (
+        <LoadingSpinnerWrapper>
+          <LoadingSpinner />
+        </LoadingSpinnerWrapper>
+      )}
     </>
   );
 }
