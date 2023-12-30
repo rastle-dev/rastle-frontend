12b049685e5f41f8b86d202d6ff637dfd413cc5a
miin-hyukkk <ham9893@naver.com>
2023-12-30 17:59:43 +0900

    refactor: 로그인 아이디,비번 input->form 태그로 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -42,49 +42,55 @@ export default function Login() {
       onChange: onChangePassword,
     },
   ];
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
   return (
-    <S.Container>
-      <S.Header>
-        <h1>R E C O R D Y&nbsp;&nbsp; S L O W</h1>
-        <h3>코디로 이해시키는 제품의 가치, @rastle_fashion</h3>
-      </S.Header>
-      <S.Wrapper>
-        {inputs.map((input) => (
-          <S.StyledInput
-            key={input.label}
-            placeholder={input.placeholder}
-            label={input.label}
-            type={input.type}
-            onChange={input.onChange}
-          />
-        ))}
-        <S.LoginButton
-          title="로그인"
-          width="100%"
-          onClick={() => {
-            if (!(email && password)) {
-              toast.dismiss();
-              errorMsg("아이디 및 비밀번호를 확인해주세요");
-            } else {
-              mutateLogin.mutate({ email, password });
-            }
-          }}
-        />
-      </S.Wrapper>
-      <S.Box>
-        {buttons.map((button, index) => (
-          <React.Fragment key={button.productName}>
-            <S.StyledButton
-              title={button.productName}
-              width={button.width}
-              onClick={button.onClick}
+    <form onSubmit={handleSubmit}>
+      <S.Container>
+        <S.Header>
+          <h1>R E C O R D Y&nbsp;&nbsp; S L O W</h1>
+          <h3>코디로 이해시키는 제품의 가치, @rastle_fashion</h3>
+        </S.Header>
+        <S.Wrapper>
+          {inputs.map((input) => (
+            <S.StyledInput
+              key={input.label}
+              placeholder={input.placeholder}
+              label={input.label}
+              type={input.type}
+              onChange={input.onChange}
             />
-            {index < buttons.length - 1 && <S.Line />}
-          </React.Fragment>
-        ))}
-      </S.Box>
-      <SNSLogin />
-    </S.Container>
+          ))}
+          <S.LoginButton
+            title="로그인"
+            width="100%"
+            onClick={() => {
+              if (!(email && password)) {
+                toast.dismiss();
+                errorMsg("아이디 및 비밀번호를 확인해주세요");
+              } else {
+                mutateLogin.mutate({ email, password });
+              }
+            }}
+          />
+        </S.Wrapper>
+        <S.Box>
+          {buttons.map((button, index) => (
+            <React.Fragment key={button.productName}>
+              <S.StyledButton
+                title={button.productName}
+                width={button.width}
+                onClick={button.onClick}
+              />
+              {index < buttons.length - 1 && <S.Line />}
+            </React.Fragment>
+          ))}
+        </S.Box>
+
+        <SNSLogin />
+      </S.Container>
+    </form>
   );
 }
 
