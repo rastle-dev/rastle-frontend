44377a2fc5c3a57a7fec6f18f895e43f8ce23b50
miin-hyukkk <ham9893@naver.com>
2023-12-30 17:55:15 +0900

    feat: ItemElement 인터페이스 옵셔널 수정

--- src/interface/itemElement.ts
+++ src/interface/itemElement.ts
@@ -1,9 +1,9 @@
 // shop에서 각 제품의 뷰(썸네일 사진, 가격, 할인된 가격 등)를 나타냅니다.
 export default interface ItemElementProps {
-  bundleId: number;
-  categoryId: number;
+  bundleId?: number;
+  categoryId?: number;
   discountPrice?: number;
-  displayOrder: 100;
+  displayOrder?: 100;
   id: number;
   mainThumbnail: string;
   subThumbnail?: string;
