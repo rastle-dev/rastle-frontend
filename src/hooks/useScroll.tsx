9171b85065c3d8bc1a24094534b761ca41be14a5
miin-hyukkk <ham9893@naver.com>
2024-01-01 15:41:24 +0900

    feat: useScroll 생성

---
+++ src/hooks/useScroll.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function useScroll() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Add smooth scrolling behavior
+    });
+  };
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+  const handleScroll = () => {
+    // 20px이상 위치했을 때 버튼 보임
+    setShowScrollButton(window.scrollY > 20);
+  };
+  return {
+    showScrollButton,
+    setShowScrollButton,
+    scrollToBottom,
+    scrollToTop,
+    handleScroll,
+  };
+}
