290a478a89abe93c8223b16237242ef88427f9c8
miin-hyukkk <ham9893@naver.com>
2024-01-15 18:53:45 +0900

    feat: 주문내역 로딩바 및 세션만료 다이얼로그 구현

--- src/components/Mypage/orderList.tsx
+++ src/components/Mypage/orderList.tsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "@/styles/mypage/orderList/index.styles";
 import { useQuery } from "@tanstack/react-query";
 import QUERYKEYS from "@/constants/querykey";
 import { loadOrderList } from "@/api/cart";
 import useOrderList from "@/hooks/mypage/orderList/useOrderList";
+import useDialog from "@/hooks/useDialog";
+import { useRouter } from "next/dist/client/router";
+import LoadingBar from "@/components/LoadingBar";
+import Dialog from "@/components/Common/Dialog";
+import PATH from "@/constants/path";
 
 export default function OrderList() {
-  const { menuList } = useOrderList();
+  const router = useRouter();
+  const { menuList, timedOut, setTimedOut } = useOrderList();
+  const { isDialogOpen, openDialog, closeDialog } = useDialog();
   const { data: orderListData, isLoading } = useQuery(
     [QUERYKEYS.LOAD_ORDER_LIST],
     loadOrderList,
   );
+  let timeoutId: NodeJS.Timeout | undefined;
+  useEffect(() => {
+    if (isLoading && timedOut) {
+      openDialog();
+    }
+  }, [timedOut]);
+  useEffect(() => {
+    if (isLoading) {
+      timeoutId = setTimeout(() => {
+        setTimedOut(true);
+      }, 5000);
+    } else {
+      setTimedOut(false);
+      clearTimeout(timeoutId);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [isLoading]);
+  if (isLoading && !timedOut) return <LoadingBar type={6} />;
   return (
-    <S.Wrap>
+    <S.Wrap isLoading={isLoading}>
+      {isDialogOpen && (
+        <Dialog
+          onClickBasketButton={() => {
+            localStorage.clear();
+            closeDialog();
+            router.push(PATH.LOGIN);
+          }}
+          visible
+          title="세션이 만료되어 로그아웃합니다."
+          refuse="확인"
+          confirm=""
+          size={40}
+        />
+      )}
       <h2>주문내역</h2>
       {orderListData?.data.content.length === 0 ? (
         <S.NODATA>아직 주문하신 상품이 없어요! 😋</S.NODATA>
