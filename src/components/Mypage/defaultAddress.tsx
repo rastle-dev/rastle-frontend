d2569ca528ee60e9a574faf88704cdf6aa9423e2
miin-hyukkk <ham9893@naver.com>
2024-01-16 19:35:03 +0900

    feat: 배송지 정보 관련 로직-> 배송지 커스텀훅으로 분리

--- src/components/Mypage/defaultAddress.tsx
+++ src/components/Mypage/defaultAddress.tsx
@@ -1,11 +1,130 @@
-import React from "react";
-import { NODATA } from "@/styles/mypage/cart/index.styles";
+import React, { useEffect, useState } from "react";
+import useLoginInfo from "@/hooks/mypage/loginInfo/useLoginInfo";
+import LoadingBar from "@/components/LoadingBar";
+import Dialog from "@/components/Common/Dialog";
+import PATH from "@/constants/path";
+import { useRouter } from "next/dist/client/router";
+import * as S from "@/styles/mypage/defaultAddress/index.styles";
+import DaumPostcode from "react-daum-postcode";
+import useOrder from "@/hooks/useOrder";
+import useDefaultAddress from "@/hooks/mypage/defaultAddress/useDefaultAddress";
+import useDialog from "@/hooks/useDialog";
 
 export default function DefaultAddress() {
+  const {
+    mutateUpdateAddressProduct,
+    data,
+    isLoading,
+    openPostcode,
+    handlePostal,
+    DefaultAddressInputs,
+  } = useDefaultAddress();
+  const { deleteUser, logout } = useLoginInfo();
+  const { openDialog, closeDialog, isDialogOpen } = useDialog();
+
+  console.log("address", data?.data);
+
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     mutateUpdateAddressProduct.mutate({
+  //       zipCode: "12345",
+  //       roadAddress: "서울특별시 강남구 테헤란로 427",
+  //       detailAddress: "테헤란로 427",
+  //     });
+  //   }
+  // }, []);
+  const router = useRouter();
+  const [timedOut, setTimedOut] = useState(false);
+  let timeoutId: NodeJS.Timeout | undefined;
+  useEffect(() => {
+    if (isLoading && timedOut) {
+      openDialog();
+    }
+  }, [timedOut]);
+  useEffect(() => {
+    if (isLoading) {
+      timeoutId = setTimeout(() => {
+        setTimedOut(true);
+      }, 5000);
+    } else {
+      setTimedOut(false);
+      clearTimeout(timeoutId);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [isLoading]);
+
+  if (isLoading && !timedOut) return <LoadingBar type={6} />;
+
+  // const inputs = [
+  //   {
+  //     label: "우편번호",
+  //     size: 75,
+  //     title: "검색하기",
+  //     onClick: handlePostal,
+  //     value: postalAddress.zonecode,
+  //   },
+  //   { label: "주소", value: postalAddress.address },
+  //   { label: "상세 주소", value: detailPostal, onChange: onChangeDetailPostal },
+  // ];
+
   return (
     <div>
+      {isDialogOpen && (
+        <Dialog
+          onClickBasketButton={() => {
+            localStorage.clear();
+            closeDialog();
+            router.push(PATH.LOGIN);
+          }}
+          visible
+          title="세션이 만료되어 로그아웃합니다."
+          refuse="확인"
+          confirm=""
+          size={34}
+        />
+      )}
       <h2>기본 배송지</h2>
-      <NODATA>첫 주문을 진행하고 기본 배송지를 등록해보세요.</NODATA>
+      <S.Wrapper>
+        {DefaultAddressInputs.map((input) => (
+          <S.DeliveryBox key={input.label}>
+            {input.size ? (
+              <S.Postal>
+                <S.DeliveryInput
+                  label={input.label}
+                  size={input.size}
+                  value={input.value}
+                  onChange={input.onChange}
+                />
+                <S.PostalButtonWrapper>
+                  <S.PostalButton
+                    title={input.title}
+                    onClick={() => handlePostal.clickButton()}
+                  />
+                </S.PostalButtonWrapper>
+              </S.Postal>
+            ) : (
+              <S.DeliveryInput
+                label={input.label}
+                size={input.size}
+                value={input.value}
+                onChange={input.onChange}
+              />
+            )}
+            {input.size && openPostcode && (
+              <DaumPostcode
+                onComplete={handlePostal.selectAddress}
+                autoClose={false}
+              />
+            )}
+          </S.DeliveryBox>
+        ))}
+      </S.Wrapper>
+      <S.DeleteButtonWrapper>
+        <S.MobileLogoutButton title="로그아웃" onClick={logout} />
+        <S.DeleteButton title="탈퇴하기" onClick={deleteUser} />
+      </S.DeleteButtonWrapper>
     </div>
   );
 }
+// 첫 주문을 진행하고 기본 배송지를 등록해보세요.
--- src/hooks/mypage/defaultAddress/useDefaultAddress.ts
+++ src/hooks/mypage/defaultAddress/useDefaultAddress.ts
@@ -4,13 +4,53 @@ import QUERYKEYS from "@/constants/querykey";
 import { loadDefaultAddress, updateDefaultAddress } from "@/api/cart";
 import { toast } from "react-toastify";
 import errorMsg from "@/components/Toast/error";
+import useInput from "@/hooks/useInput";
+import { useState } from "react";
+
+type Address = {
+  address: string | undefined;
+  zonecode: number | undefined;
+};
+type CommonInputField = {
+  label: string;
+  size?: number;
+  title?: string;
+  onClick?: {
+    clickButton: () => void;
+    selectAddress: (addressData: any) => void;
+  };
+  value?: any;
+  onChange?: any;
+};
 
 export default function useDefaultAddress() {
   const queryClient = useQueryClient();
+  const [receiver, onChangeReceiver, setReceiver] = useInput("");
+  const [detailPostal, onChangeDetailPostal] = useInput("");
+  const [phoneNumber, onChangePhoneNumber, setPhoneNumber] = useInput("");
+  const [openPostcode, setOpenPostcode] = useState<boolean>(false);
+  const [postalAddress, setAddress] = useState<Address>({
+    address: undefined,
+    zonecode: undefined,
+  });
   const { data, isLoading } = useQuery({
     queryKey: [QUERYKEYS.LOAD_DEFAULT_ADDRESS],
     queryFn: loadDefaultAddress,
   });
+  const handlePostal = {
+    // 버튼 클릭 이벤트
+    clickButton: () => {
+      setOpenPostcode((current) => !current);
+    },
+    // 주소 선택 이벤트
+    selectAddress: (addressData: any) => {
+      setAddress({
+        address: addressData.address,
+        zonecode: addressData.zonecode,
+      });
+      setOpenPostcode(false);
+    },
+  };
   const mutateUpdateAddressProduct = useMutation(
     ["updateDefaultAddress"],
     updateDefaultAddress,
@@ -30,9 +70,39 @@ export default function useDefaultAddress() {
       },
     },
   );
+  const commonInputFields: CommonInputField[] = [
+    {
+      label: "우편번호",
+      size: 75,
+      title: "검색하기",
+      onClick: handlePostal,
+      value: postalAddress.zonecode,
+    },
+    { label: "주소", value: postalAddress.address },
+    { label: "상세 주소", value: detailPostal, onChange: onChangeDetailPostal },
+  ];
+
+  const OrderInputs = [
+    ...commonInputFields,
+    {
+      label: "받는 분",
+      onChange: onChangeReceiver,
+      value: receiver,
+    },
+    {
+      label: "연락처",
+      onChange: onChangePhoneNumber,
+      value: phoneNumber,
+    },
+  ];
+  const DefaultAddressInputs = [...commonInputFields];
   return {
     mutateUpdateAddressProduct,
     data,
     isLoading,
+    openPostcode,
+    DefaultAddressInputs,
+    OrderInputs,
+    handlePostal,
   };
 }
