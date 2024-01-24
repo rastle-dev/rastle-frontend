b66b7ed7aa326c109a1ef0e3343319d1c3d95bc0
miin-hyukkk <ham9893@naver.com>
2024-01-24 17:51:22 +0900

    feat: 로그인 페이지 테스트 코드 구현

---
+++ __test__/pages/login/index.test.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import Login from "@/pages/login/index";
+import { RecoilRoot } from "recoil";
+import ErrorBoundary from "@/components/ErrorBoundary";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+jest.mock("next/dist/client/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+function renderLogin() {
+  const mutateLogin = jest.fn();
+
+  const testUtils = render(
+    <ErrorBoundary>
+      <QueryClientProvider client={new QueryClient()}>
+        <RecoilRoot>
+          <Hydrate>
+            <Login />
+          </Hydrate>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </ErrorBoundary>,
+  );
+
+  const Email = () => testUtils.getByPlaceholderText("예) rastle@rastle.com");
+  const Password = () => testUtils.getByPlaceholderText("••••••••");
+
+  const typeEmail = (email: string) => {
+    const emailInput = Email();
+    fireEvent.change(emailInput, { target: { value: email } });
+  };
+  // const typePassword = (password: string) => {
+  //   //   const passwordInput = Password();
+  //   //   userEvent.type(passwordInput, password);
+  //   // };
+  const typePassword = (password: string) => {
+    const passwordInput = Password();
+    fireEvent.input(passwordInput, { target: { value: password } });
+  };
+
+  const LoginButton = () =>
+    testUtils.getByText("로그인", { selector: "button" });
+
+  const onClickLoginButton = () => {
+    userEvent.click(LoginButton());
+    const email = (Email() as HTMLInputElement).value;
+    const password = (Password() as HTMLInputElement).value;
+    mutateLogin({ email, password });
+  };
+  return {
+    Email,
+    Password,
+    testUtils,
+    mutateLogin,
+    typeEmail,
+    typePassword,
+    LoginButton,
+    onClickLoginButton,
+  };
+}
+describe("로그인 페이지 테스트", () => {
+  it("로그인 페이지 스냅샷 테스트", () => {
+    const { testUtils } = renderLogin();
+    expect(testUtils.container).toMatchSnapshot();
+  });
+  it("로그인 페이지 렌더링 테스트", () => {
+    const { Email, Password, LoginButton } = renderLogin();
+    expect(Email()).toBeInTheDocument();
+    expect(Password()).toBeInTheDocument();
+    expect(LoginButton()).toBeInTheDocument();
+  });
+  it("mutateLogin 함수 테스트", async () => {
+    const {
+      Email,
+      Password,
+      typeEmail,
+      typePassword,
+      onClickLoginButton,
+      mutateLogin,
+    } = renderLogin();
+
+    typeEmail("minhyuk9893@google.com");
+    typePassword("ham061625@");
+
+    onClickLoginButton();
+    expect((Email() as HTMLInputElement).value).toBe("minhyuk9893@google.com");
+    expect((Password() as HTMLInputElement).value).toBe("ham061625@");
+
+    expect(mutateLogin).toHaveBeenCalledWith({
+      email: "minhyuk9893@google.com",
+      password: "ham061625@",
+    });
+  });
+});
