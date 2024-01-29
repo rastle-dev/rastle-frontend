222d9a0a6700b76185625977427c3ac4bb610665
miin-hyukkk <ham9893@naver.com>
2024-01-29 17:40:23 +0900

    feat: 정규식으로 입력창 문자열 제한

--- src/components/Event/EnterEventModal.tsx
+++ src/components/Event/EnterEventModal.tsx
@@ -81,13 +81,13 @@ export default function EnterEventModal({
       label: "전화번호",
       id: "phone",
       onChange: onChangeEventPhoneNumber,
-      value: eventPhoneNumber,
+      value: eventPhoneNumber.replace(/\D/g, "").slice(0, 11),
     },
     {
       label: "인스타그램",
       id: "instagram",
       onChange: onChangeInstagramId,
-      value: instagramId,
+      value: instagramId.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g, ""),
     },
   ];
   const mutateApplyEvent = useMutation(["applyEvent"], applyEvent, {
