b56fb1d3b0a7e547fcb15a7bd88ace774d2759de
miin-hyukkk <ham9893@naver.com>
2024-02-21 21:47:23 +0900

    feat: 제품상세정보 인터페이스 추가

--- src/interface/product/detailProduct.ts
+++ src/interface/product/detailProduct.ts
@@ -1,3 +1,26 @@
+export interface ProductDetailData {
+  bundleId?: number;
+  categoryId?: number;
+  detailImage: {
+    imageUrls: string[];
+  };
+  discountPrice: number;
+  displayOrder: number;
+  eventApplyCount: number;
+  eventId: number;
+  id: number;
+  mainImage: {
+    imageUrls: string[];
+  };
+  mainThumbnailImage: string;
+  name: string;
+  price: number;
+  productColor: {
+    productColors: Color[];
+  };
+  subThumbnailImage: "string";
+  visible: boolean;
+}
 export type Color = {
   color: string;
   sizes: Size[];
