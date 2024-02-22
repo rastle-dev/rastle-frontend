937a21b61f8db82173b6d67d7b5b50a70de488ef
miin-hyukkk <ham9893@naver.com>
2024-02-22 20:23:15 +0900

    feat: 제품상세,이벤트상세페이지 SSR 구현

---
+++ src/components/Product/commonServerSideProps.ts
@@ -0,0 +1,27 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import QUERYKEYS from "@/constants/querykey";
+import { loadProductDetail } from "@/api/shop";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+
+const commonServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const queryClient = new QueryClient();
+  const { productId } = context.query; // context에서 productId를 가져옵니다.
+  console.log("context", productId);
+  // Prefetch queries
+  if (productId) {
+    const numericProductId = Number(productId);
+    await queryClient.prefetchQuery(
+      [QUERYKEYS.LOAD_PRODUCT_DETAIL, numericProductId],
+      () => loadProductDetail(numericProductId),
+    );
+  }
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export default commonServerSideProps;
