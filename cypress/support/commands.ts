b3af69212cf9b3d80b5ce9f5b9668222d67840f7
miin-hyukkk <ham9893@naver.com>
2024-03-05 00:06:48 +0900

    feat: 로그인 기능 재사용 가능 커맨드로 구현

---
+++ cypress/support/commands.ts
@@ -0,0 +1,56 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+Cypress.Commands.add("login", () => {
+  cy.request({
+    url: `https://api.recordyslow.com/auth/login`,
+    method: "POST",
+    body: {
+      email: `${Cypress.env("CYPRESS_TEST_EMAIL")}`,
+      password: `${Cypress.env("CYPRESS_TEST_PASSWORD")}`,
+    },
+  })
+    .its("headers")
+    .then((response) => {
+      localStorage.setItem(
+        "accessToken",
+        // @ts-expect-error 이 부분은 의도적으로 에러를 억제하기 위해 사용되었습니다.
+        `${response.authorization.replace("Bearer ", "")}`,
+      );
+    });
+});
