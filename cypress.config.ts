3c5cc374faa69c11ac644c3d03bdc28e2b5bf3bd
miin-hyukkk <ham9893@naver.com>
2024-03-05 01:41:23 +0900

    config: cypress.yml, cypress.config.ts 수정

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "77qb41",
-  env: {
-    CYPRESS_API_HOST: process.env.NEXT_PUBLIC_BASE_URL,
-    baseUrl: "http://localhost:3000",
-    "cypress-react-selector": {
-      root: "#root",
-    },
-  },
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
