8fbe6614b6489a399b22ff1f5cda45578c574730
miin-hyukkk <ham9893@naver.com>
2024-03-07 17:17:30 +0900

    feat: 바로구매, 장바구니구매 구분하여 테스트 코드 작성

--- cypress/e2e/3-purchase/purchase.spec.cy.ts
+++ cypress/e2e/3-purchase/purchase.spec.cy.ts
@@ -6,69 +6,100 @@ describe("purchase e2e test", () => {
       cy.intercept(
         {
           method: "GET",
-          url: `${API.CART}`,
+          url: `${API.MEMBER}`,
         },
-        { fixture: "cartProduct.json" },
+        { fixture: "memberInfo.json" },
       );
-      cy.visit(
-        "http://localhost:3000/mypage?tab=%EC%9E%A5%EB%B0%94%EA%B5%AC%EB%8B%88",
+      cy.intercept(
+        {
+          method: "GET",
+          url: `${API.LOAD_ADDRESS}`,
+        },
+        { fixture: "defaultAddress.json" },
       );
+      cy.intercept(
+        {
+          method: "GET",
+          url: `${API.COUPON}`,
+        },
+        { fixture: "coupon.json" },
+      );
+      cy.intercept(
+        {
+          method: "POST",
+          url: `${API.PAYMENTPREPARE}`,
+        },
+        { fixture: "preparePayment.json" },
+      ).as("paymentPrepare");
     });
+    // cy.intercept({
+    //   method: "PUT",
+    //   url: `${API.UPDATE_ADDRESS}`,
+    // });
+    // cy.intercept({
+    //   method: "PUT",
+    //   url: `${API.UPDATE_PHONENUMBER}`,
+    // });
   });
 
-  it("장바구니에 있는 상품 한 개 구매하기 사전 검증 성공 확인 및 pg사 호출 테스트", () => {
+  it("장바구니에 있는 상품 전체 구매하기 사전 검증 성공 확인 및 pg사 호출 테스트", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: `${API.CART}`,
+      },
+      { fixture: "cartProducts.json" },
+    );
     cy.intercept(
       {
         method: "POST",
         url: `${API.ORDERS}`,
       },
-      { fixture: "virtualOrder.json" },
+      { fixture: "virtualCartOrder.json" },
     );
+    cy.intercept("GET", "https://service.iamport.kr/users/pg/imp47805780", {
+      fixture: "productDetail.json",
+    });
+    cy.visit(
+      "http://localhost:3000/mypage?tab=%EC%9E%A5%EB%B0%94%EA%B5%AC%EB%8B%88",
+    );
+    cy.contains("전체상품 주문").click();
+    cy.contains("사용가능한 쿠폰이 1장 있어요.").click();
+    cy.contains("테스트 쿠폰").click();
+    cy.contains("일반결제").click();
+    cy.contains("결제하기").click();
+    cy.wait("@paymentPrepare").then((interception) => {
+      expect(interception?.response?.statusCode).to.equal(200);
+    });
+  });
+  it("상품 상세페이지에서 바로 구매하기 사전 검증 성공 확인 및 pg사 호출 테스트", () => {
+    cy.intercept("GET", "/_next/data/development/product.json?productId=14", {
+      fixture: "productDetail.json",
+    });
     cy.intercept(
       {
         method: "GET",
-        url: `${API.MEMBER}`,
+        url: `${API.CART}`,
       },
-      { fixture: "memberInfo.json" },
-    );
-    cy.intercept(
-      {
-        method: "GET",
-        url: `${API.LOAD_ADDRESS}`,
-      },
-      { fixture: "defaultAddress.json" },
-    );
-    cy.intercept(
-      {
-        method: "GET",
-        url: `${API.COUPON}`,
-      },
-      { fixture: "coupon.json" },
+      { fixture: "directProduct.json" },
     );
     cy.intercept(
       {
         method: "POST",
-        url: `${API.PAYMENTPREPARE}`,
+        url: `${API.ORDERS}`,
       },
-      {
-        merchant_uid: "17097382350326",
-        couponId: 1,
-        deliveryPrice: 3000,
-      },
+      { fixture: "virtualDirectOrder.json" },
     );
-    cy.intercept({
-      method: "PUT",
-      url: `${API.UPDATE_ADDRESS}`,
-    });
-    cy.intercept({
-      method: "PUT",
-      url: `${API.UPDATE_PHONENUMBER}`,
-    });
-    // cy.contains("와이드 로우 핀턱 팬츠 연그레이").parent().click();
-    cy.get("[data-cy='purchase-button']").click();
+    cy.contains("와이드 로우 핀턱 팬츠 연그레이").click();
+    cy.get("[data-cy='color-button']").click();
+    cy.get("[data-cy='size-button']").eq(0).click();
+    cy.contains("구매하기").click();
     cy.contains("사용가능한 쿠폰이 1장 있어요.").click();
     cy.contains("테스트 쿠폰").click();
-    cy.contains("카카오페이").click();
+    cy.contains("일반결제").click();
     cy.contains("결제하기").click();
+    cy.wait("@paymentPrepare").then((interception) => {
+      expect(interception?.response?.statusCode).to.equal(200);
+    });
   });
 });
