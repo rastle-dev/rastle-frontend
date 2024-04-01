60d505678a55bc32e8a10f8c0b278fbda2dc10fc
miin-hyukkk <ham9893@naver.com>
2024-04-01 14:33:53 +0900

    feat: 주문 취소 제품 인터페이스 구현

---
+++ src/interface/Cancel/SelectedItem.ts
@@ -0,0 +1,12 @@
+export interface SelectedItem {
+  color: string | undefined;
+  count: number;
+  prevCount: number;
+  name: string;
+  price: number;
+  productId: number;
+  productOrderNumber: number;
+  size: string;
+  thumbnailUrl: string;
+  totalPrice: number;
+}
