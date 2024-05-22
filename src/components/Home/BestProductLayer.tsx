eacab966580b617c3df5bc78e413c3a561db9de4
miin-hyukkk <ham9893@naver.com>
2024-05-22 16:07:17 +0900

    feat: 베스트 상품 모바일일 경우 뷰포인트에 6개로 구현

--- src/components/Home/BestProductLayer.tsx
+++ src/components/Home/BestProductLayer.tsx
@@ -2,7 +2,7 @@ import * as S from "@/styles/index/index.styles";
 import ItemElementProps from "@/interface/itemElement";
 import ItemElement from "@/components/ItemElement";
 import LazyLink from "@/components/LazyLink";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface ProductData {
   data: {
@@ -10,35 +10,50 @@ interface ProductData {
   };
 }
 function BestProductLayer({ productData }: { productData: ProductData }) {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+    checkIsMobile();
+    window.addEventListener("resize", checkIsMobile);
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
   return (
     <S.ProductWrapper>
       <S.BestProductTitle>BEST</S.BestProductTitle>
       <S.ProductDesc>한달동안 가장 많은 사랑을 받은 제품 입니다.</S.ProductDesc>
       <S.BestItemContainer>
-        {productData?.data.content.map((item: ItemElementProps) => {
-          // 만약 price와 discountPrice가 같으면 discountPrice 필드를 없애고, 그렇지 않으면 그대로 유지
-          const finalDiscountPrice =
-            item.price === item.discountPrice ? undefined : item.discountPrice;
+        {productData?.data.content
+          .slice(0, isMobile ? 6 : 5)
+          .map((item: ItemElementProps) => {
+            // 만약 price와 discountPrice가 같으면 discountPrice 필드를 없애고, 그렇지 않으면 그대로 유지
+            const finalDiscountPrice =
+              item.price === item.discountPrice
+                ? undefined
+                : item.discountPrice;
 
-          return (
-            <ItemElement
-              key={item.id}
-              mainThumbnail={item.mainThumbnail}
-              subThumbnail={item.subThumbnail}
-              name={item.name}
-              price={item.price}
-              productName={item.productName}
-              discountPrice={finalDiscountPrice}
-              id={item.id}
-              categoryId={item.categoryId}
-              isEvent={!!item.eventId}
-            />
-          );
-        })}
+            return (
+              <ItemElement
+                key={item.id}
+                mainThumbnail={item.mainThumbnail}
+                subThumbnail={item.subThumbnail}
+                name={item.name}
+                price={item.price}
+                productName={item.productName}
+                discountPrice={finalDiscountPrice}
+                id={item.id}
+                categoryId={item.categoryId}
+                isEvent={!!item.eventId}
+              />
+            );
+          })}
       </S.BestItemContainer>
       <S.ViewMore>
         <LazyLink href="/shop" title="레코디슬로우 전체 상품 페이지">
-          더 많은 상품 보러가기
+          {">"} 베스트 상품 더보기
         </LazyLink>
       </S.ViewMore>
     </S.ProductWrapper>
