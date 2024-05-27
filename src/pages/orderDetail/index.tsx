3a0eb63d30072821f764743fa581980c0d73e812
miin-hyukkk <ham9893@naver.com>
2024-05-27 16:58:46 +0900

    chore: 주석 제거

--- src/pages/orderDetail/index.tsx
+++ src/pages/orderDetail/index.tsx
@@ -67,15 +67,6 @@ export default function OrderDetail() {
 
   return (
     <S.Temp>
-      {/* <style> */}
-      {/*   {` */}
-      {/*     @media (min-width: 1px) and (max-width: 767px) { */}
-      {/*       html{ */}
-      {/*         font-size: 11px; */}
-      {/*       } */}
-      {/*     } */}
-      {/*   `} */}
-      {/* </style> */}
       <S.Container>
         <S.MainTitle>주문 상세조회</S.MainTitle>
         <S.InfoWrapper>
