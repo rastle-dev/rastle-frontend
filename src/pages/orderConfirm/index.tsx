86f8640bb0485e24cbb63b085d25c4dfccd57664
miin-hyukkk <ham9893@naver.com>
2024-05-29 15:53:55 +0900

    feat: 결제 실패 페이지 구현

--- src/pages/orderConfirm/index.tsx
+++ src/pages/orderConfirm/index.tsx
@@ -3,61 +3,67 @@ import * as S from "@/styles/orderconfirm/index.styles";
 import useOrderConfirm from "@/hooks/useOrderConfirm";
 import PATH from "@/constants/path";
 import { useRouter } from "next/router";
+import OrderFail from "@/components/Order/OrderFail";
 
 export default function OrderConfirm() {
   const router = useRouter();
-  const { ProductList, OrdererInfo, receiverData } = useOrderConfirm();
+  const { ProductList, OrdererInfo, receiverData, errorMsg } =
+    useOrderConfirm();
   return (
     <S.Temp>
-      <S.Container>
-        <S.Header>
-          <h1>주문이 완료되었습니다! 😄</h1>
-        </S.Header>
-        <S.InfoWrapper>
-          <h2>제품 정보</h2>
-          {ProductList.map((item) => (
-            <S.Product>
-              <S.Thumbnail
-                src={item.mainThumbnailImage}
-                alt={item.mainThumbnailImage}
-              />
-              <S.Info>
-                <S.ProductName>{item.title}</S.ProductName>
-                <S.NumPrice>
-                  {item.count}개 / {item.price}원
-                </S.NumPrice>
-                <S.SizeColor>
-                  {item.size} / {item.color}
-                </S.SizeColor>
-              </S.Info>
-            </S.Product>
-          ))}
-          <h2>주문 정보</h2>
-          <S.OrdererInfo>
-            {OrdererInfo.map((info) => (
-              <S.Box>
-                <S.Meta>{info.meta}</S.Meta>
-                <S.Data>{info.data}</S.Data>
-              </S.Box>
+      {errorMsg ? (
+        <OrderFail />
+      ) : (
+        <S.Container>
+          <S.Header>
+            <h1>주문이 완료되었습니다! 😄</h1>
+          </S.Header>
+          <S.InfoWrapper>
+            <h2>제품 정보</h2>
+            {ProductList.map((item) => (
+              <S.Product>
+                <S.Thumbnail
+                  src={item.mainThumbnailImage}
+                  alt={item.mainThumbnailImage}
+                />
+                <S.Info>
+                  <S.ProductName>{item.title}</S.ProductName>
+                  <S.NumPrice>
+                    {item.count}개 / {item.price}원
+                  </S.NumPrice>
+                  <S.SizeColor>
+                    {item.size} / {item.color}
+                  </S.SizeColor>
+                </S.Info>
+              </S.Product>
             ))}
-          </S.OrdererInfo>
-          <S.Total>
-            <S.TotalInfo>결제 금액</S.TotalInfo>
-            <S.TotalPrice>
-              {receiverData?.data.paymentAmount.toLocaleString()}원
-            </S.TotalPrice>
-          </S.Total>
-          <S.ButtonDiv>
-            <S.StyledBuyButton
-              title="쇼핑하러 가기"
-              type="shop"
-              onClick={() => {
-                router.push(PATH.SHOP);
-              }}
-            />
-          </S.ButtonDiv>
-        </S.InfoWrapper>
-      </S.Container>
+            <h2>주문 정보</h2>
+            <S.OrdererInfo>
+              {OrdererInfo.map((info) => (
+                <S.Box>
+                  <S.Meta>{info.meta}</S.Meta>
+                  <S.Data>{info.data}</S.Data>
+                </S.Box>
+              ))}
+            </S.OrdererInfo>
+            <S.Total>
+              <S.TotalInfo>결제 금액</S.TotalInfo>
+              <S.TotalPrice>
+                {receiverData?.data.paymentAmount.toLocaleString()}원
+              </S.TotalPrice>
+            </S.Total>
+            <S.ButtonDiv>
+              <S.StyledBuyButton
+                title="쇼핑하러 가기"
+                type="shop"
+                onClick={() => {
+                  router.push(PATH.SHOP);
+                }}
+              />
+            </S.ButtonDiv>
+          </S.InfoWrapper>
+        </S.Container>
+      )}
     </S.Temp>
   );
 }
