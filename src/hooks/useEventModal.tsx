0612fd833568c5937ea6383ebac3a97389d66cbf
miin-hyukkk <ham9893@naver.com>
2024-07-02 17:39:10 +0900

    refactor: 응모내역데이터 불러오기 오류 수정

--- src/hooks/useEventModal.tsx
+++ src/hooks/useEventModal.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { applyEvent } from "@/api/shop";
 import { toast } from "react-toastify";
 import errorMsg from "@/components/Toast/error";
@@ -7,18 +7,26 @@ import QUERYKEYS from "@/constants/querykey";
 import { useRecoilState } from "recoil";
 import { eventDialogState, eventModalState } from "@/stores/atom/recoilState";
 import useInput from "@/hooks/useInput";
-import useEventHistory from "@/hooks/mypage/orderList/useEventHistory";
+import { useEffect } from "react";
+import { loadEventHistory } from "@/api/cart";
 
 export default function useEventModal() {
   const [, setIsEventModalOpen] = useRecoilState(eventModalState);
   const [, setIsEventDialogOpen] = useRecoilState(eventDialogState);
-  const { eventHistoryData } = useEventHistory();
+  const { data: eventHistoryData } = useQuery(
+    [QUERYKEYS.LOAD_EVENT_HISTORY],
+    () => loadEventHistory({ page: 0, size: 24 }),
+    {
+      staleTime: 5 * 60 * 1000, // 데이터가 신선한 상태로 유지될 시간 (5분)
+      cacheTime: 10 * 60 * 1000, // 캐시가 유지될 시간 (10분)
+      refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리패치 비활성화
+    },
+  );
   const lastContentItem =
     eventHistoryData?.data.content[eventHistoryData.data.content.length - 1];
-  const [eventPhoneNumber, onChangeEventPhoneNumber] = useInput(
-    lastContentItem?.eventPhoneNumber,
-  );
-  const [instagramId, onChangeInstagramId] = useInput(
+  const [eventPhoneNumber, onChangeEventPhoneNumber, setEventPhoneNumber] =
+    useInput(lastContentItem?.eventPhoneNumber);
+  const [instagramId, onChangeInstagramId, setInstagramId] = useInput(
     lastContentItem?.instagramId,
   );
   const queryClient = useQueryClient();
@@ -58,6 +66,13 @@ export default function useEventModal() {
       setIsEventDialogOpen(true);
     },
   });
+
+  useEffect(() => {
+    if (lastContentItem) {
+      setEventPhoneNumber(lastContentItem?.eventPhoneNumber);
+      setInstagramId(lastContentItem?.instagramId);
+    }
+  }, [lastContentItem, setIsEventModalOpen]);
   return {
     mutateApplyEvent,
     inputFields,
